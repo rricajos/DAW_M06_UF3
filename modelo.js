6a863dcb0e29e5202e0be8caccaa6e4e7894c868
Ricard Penin Honrubia <ergoegos@gmail.com>
2024-11-21 13:59:51 +0100

    DONNE

--- modelo.js
+++ modelo.js
@@ -1,12 +1,15 @@
-// modelo.js
-
 // Valida el nombre de usuario
+//  El nombre de usuario debe tener entre 5 y 15 caracteres, solo puede contener letras
+// (mayúsculas o minúsculas) y un guion bajo como máximo que nunca pueda estar ni al
+// principio ni al final
 function validarUsuario(usuario) {
   const regex = /^(?!_)[a-zA-Z_]{5,15}(?<!_)$/;
   return regex.test(usuario);
 }
 
-// Valida la contraseña
+// Valida la contraseña:
+// • La contraseña debe ser de 8 caracteres, debe contener al menos una letra mayúscula,
+// una letra minúscula, un número y un carácter especial, y no podrá tener espacios.
 function validarContraseña(contraseña) {
   const regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_])[^\s]{8}$/;
   return regex.test(contraseña);
